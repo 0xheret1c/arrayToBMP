ca0813109a0c2590aa6a567e54d925b412732cf1
Marius <9764028+0xheret1c@users.noreply.github.com>
2018-07-04 02:02:24 +0200

    fixed stuff!

--- ArrayToBMP/Main.cpp
+++ ArrayToBMP/Main.cpp
@@ -2,14 +2,16 @@
 
 int main()
 {
-
-	rgb24_t colors[1000* 1000];
-	for (size_t i = 0; i < 1000 * 1000; i++)
+	size_t size = 1000;
+	size_t size2 = size * size;
+	rgb24_t* colors = new rgb24_t[size2];
+	for (size_t i = 0; i < size2; i++)
 	{
-		colors[i] = (rgb24_t)i;
+		colors[i] = (rgb24_t)(i * 3.14159265359);
 	}
 	
-	arrayToBMP::ArrayToBMP(colors, 1000, 1000,".\\test.bmp");
+	arrayToBMP::ArrayToBMP(colors, size, size,".\\test.bmp");
 	std::cout << "finished" << std::endl;
+	delete[] colors;
 	return 0;
 }
